d310d183b306588d6568b49b70370600ad9c8723
PrakrutiRao <39726225+PrakrutiRao@users.noreply.github.com>
2020-04-17 00:40:52 +0530

    Added App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, Component} from 'react';
+import { Container } from 'semantic-ui-react';
+import { Time } from './not used/Time.js';
+import { InputForm } from './not used/Form.js';
+import { Img } from './components/Image.js';
+import FileUpload from './components/FileUpload';
+import FileDownload from './components/FileDownload.js';
+
+import FormOuter from './components/FormOuter.js';
+import './App.css';
+
+function App(){
+  return(
+    <FormOuter />
+  );
+}
+export default App;
+
